7d2f8603173c78ce64739169c5ca703da9b69c6a
David Halmy <David.Halmy@us.kbr.com>
2024-06-06 11:36:09 -0400

    remove most api calls

--- python/flask-app-2.0/app.py
+++ python/flask-app-2.0/app.py
@@ -13,43 +13,5 @@ def generate_id():
 def get_books():
     return jsonify({'books': books})
 
-# endpoint to add a new book
-@app.route('/reading-list/books', methods=['POST'])
-def add_book():
-    data = request.get_json()
-    book = {
-        'id': generate_id(),
-        'author': data['author'],
-        'name': data['name'],
-        'status': data['status']
-    }
-    books.append(book)
-    return jsonify(book)
-
-# endpoint to get a book by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['GET'])
-def get_book(book_id):
-    for book in books:
-        if book['id'] == book_id:
-            return jsonify(book)
-    return '', 404
-
-# endpoint to delete a book by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['DELETE'])
-def delete_book(book_id):
-    global books
-    books = [book for book in books if book['id'] != book_id]
-    return '', 204
-
-# endpoint to update a book status by ID
-@app.route('/reading-list/books/<int:book_id>', methods=['PUT'])
-def update_book_status(book_id):
-    data = request.get_json()
-    for book in books:
-        if book['id'] == book_id:
-            book['status'] = data['status']
-            return jsonify(book)
-    return '', 404
-
 if __name__ == '__main__':
     app.run()
