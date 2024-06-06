4fc82ae20b02ca1da5952ea243ed419cb7dbb0c8
David Halmy <David.Halmy@us.kbr.com>
2024-06-06 11:43:20 -0400

    remove fluff from dockerfile

--- python/flask-app-2.0/app.py
+++ python/flask-app-2.0/app.py
@@ -11,10 +11,6 @@ books = [
     }
   ]
 
-# helper function to generate IDs
-def generate_id():
-    return len(books) + 1
-
 # endpoint to get all books
 @app.route('/reading-list/books', methods=['GET'])
 def get_books():
