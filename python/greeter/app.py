39faa575cd7b08f29e130fb52fe825b9a8ecc9cf
David Halmy <David.Halmy@us.kbr.com>
2024-06-06 12:10:52 -0400

    is this it

--- python/greeter/app.py
+++ python/greeter/app.py
@@ -3,19 +3,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # endpoint to send greeting
-books = [
-    {
-      "author": "me",
-      "id": 1,
-      "name": "adam sandler",
-      "status": "read"
-    }
-  ]
 
 
 @app.route('/greeter/hello', methods=['GET'])
 def sayHello():
-    return jsonify({'books': books})
+    return "Hello user"
 
 
 if __name__ == '__main__':
